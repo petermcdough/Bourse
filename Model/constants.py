aced04c0223b2765dd43422e0abfbaef9602b849
petermcdough <hugo.fayolle@gmail.com>
2018-10-10 20:42:02 +0200

    Commit

--- Model/constants.py
+++ Model/constants.py
@@ -12,13 +12,17 @@ API_KEY = "WkHc4vJGHBT4Xtuma14T" # API key to access quandl
 MARKET = 'EURONEXT/'             # prefix of the code of a stock
 TAX = 0.0004                     # tax (percentage) applied when buying / selling stock
 FIGSIZE = (15,6)                 # dimensions of plots
-SAVE_FOLDER = "/Users/hugofayolle/Desktop/Bourse/Bourse_test/Data/"
+LOCAL = True
+if LOCAL:    
+    SAVE_FOLDER = "/Users/hugofayolle/Desktop/Bourse/Bourse/Data/"
+else:
+    SAVE_FOLDER = "/home/hugofayolle/Bourse/Data/"
 TEST_FOLDER = SAVE_FOLDER + "Test/Test_set/"
 START_DATE = "2014-01-01"
 ENVIRONMENT = Env.DEV
 SELECTED_STRATEGY = 'SMA80_SMA150'
-EMAIL = 'hugo.fayolle@gmail.com'
-PWD = 'cEFwQXBpY2gwdQ=='
+EMAIL = 'fdjc6590@gmail.com'
+PWD = 'cEFzU3cwcmQ='
 
 if ENVIRONMENT == Env.TEST:
     TEST_REFRESH_DATE_EMPTY = '2018-09-21'
--- Model/env.py
+++ Model/env.py
@@ -6,10 +6,6 @@ Created on Sun Jun 10 18:49:39 2018
 @author: hugofayolle
 """
 
-import sys
-
-sys.path.insert(0, "/Users/hugofayolle/anaconda3/lib/python3.6/")
-
 from enum import Enum
 
 class Env(Enum):
